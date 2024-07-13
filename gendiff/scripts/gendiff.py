895d29a6fe5eccbc67490f3bef961531be20b89e
kamusia <adamhorolov36027@gmail.com>
2024-07-13 11:41:07 +0300

    +format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,7 @@ def main():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', help='Path to the first file')
     parser.add_argument('second_file', help='Path to the second file')
+    parser.add_argument('-f', '--format', help='Set format of output')
 
     args = parser.parse_args()
 
