de0570b6edd70661c08ef7c776e9dd48ff13e0f4
kamusia <adamhorolov36027@gmail.com>
2024-07-27 11:56:46 +0300

    dix tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,18 +1,8 @@
 from gendiff.main import generate_diff
-import tempfile
 
 
 def test_gendiff_json():
     assert generate_diff('file1.json', 'file2.json') == '''host: hexlet.io
-- proxy: 123.234.53.22
-- follow: False
-- timeout: 50
-+ timeout: 20
-+ verbose: True'''
-
-
-def test_gendiff_json():
-    assert generate_diff('filepath1.yml', 'filepath2.yml') == '''host: hexlet.io
 - proxy: 123.234.53.22
 - follow: False
 - timeout: 50
