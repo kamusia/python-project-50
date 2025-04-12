ff798632a8354183f6e40439a3b30a9ea616f65a
kamusia <adamhorolov36027@gmail.com>
2025-04-12 14:10:51 +0300

    fix all tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -10,6 +10,7 @@ json_new2 = 'tests/fixtures/nested_file2.json'
 yml_old2 = 'tests/fixtures/nested_file1.yml'
 yml_new2 = 'tests/fixtures/nested_file2.yml'
 stylish = 'tests/fixtures/stylish'
+plain = 'tests/fixtures/plain'
 nested_stylish = 'tests/fixtures/nested_stylish'
 nested_plain = 'tests/fixtures/nested_plain'
 nested_json = 'tests/fixtures/nested_json.json'
@@ -22,8 +23,8 @@ nested_json = 'tests/fixtures/nested_json.json'
         (yaml_old, yaml_new, 'stylish', stylish),
         (json_old2, json_new2, 'stylish', nested_stylish),
         (yml_old2, yml_new2, 'stylish', nested_stylish),
-        (json_old, json_new, 'plain', nested_plain),
-        (yaml_old, yaml_new, 'plain', nested_plain),
+        (json_old, json_new, 'plain', plain),
+        (yaml_old, yaml_new, 'plain', plain),
         (json_old2, json_new2, 'plain', nested_plain),
         (yml_old2, yml_new2, 'plain', nested_plain),
         (json_old2, json_new2, 'json', nested_json)
